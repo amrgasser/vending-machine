f74429b165a85806bcd857f7d9ae70b1bdb91e7d
amrgasser <amr_gasser@hotmail.com>
2022-01-02 02:08:05 +0200

    Username unique

--- src/main/java/com/example/Flapkap/model/User.java
+++ src/main/java/com/example/Flapkap/model/User.java
@@ -15,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
 
-    @Column(name="username")
+    @Column(name="username",unique = true)
     private String username;
 
     @Column(name="password")
